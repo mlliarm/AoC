7e8257470f54f6807762f5c037ef7a030737a27a
milia <mlliarm@users.noreply.github.com>
2023-12-08 04:53:17 +0100

    part 2

--- 2023/Day07/camelCards.py
+++ 2023/Day07/camelCards.py
@@ -18,6 +18,22 @@ cards = {
     "2": 1,
 }
 
+j_cards = {
+    "A": 13,
+    "K": 12,
+    "Q": 11,
+    "T": 10,
+    "9": 9,
+    "8": 8,
+    "7": 7,
+    "6": 6,
+    "5": 5,
+    "4": 4,
+    "3": 3,
+    "2": 2,
+    "J": 1,
+}
+   
 types_of_hands = {
         "[5]":("Five of a kind",7),
          "[4, 1]":("Four of a kind",6),
@@ -41,15 +57,19 @@ class Hand:
         sod = dict_to_odict(c)
         self.multiplicity = sod
         
-    def set_hand_name(self) -> None:
+    def set_hand_name_J(self) -> None:
         if 'J' in self.content:
             temp_hand_cont = self.content
             thc = sorted(temp_hand_cont, key=lambda x: cards[x], reverse=True)
-            print(thc)
-            if self.content.index('J') == 0:
-                thc = ("".join(thc)).replace("J",thc[1])
-            else:
-                thc = ("".join(thc)).replace("J",thc[0])
+            c = check_freq(self.content)           
+            max_card = list(max(c, key=lambda x:c[x]))[0]
+            if self.content != 'JJJJJ':
+                c2 = removekey(c,max_card)
+                second_max_card = list(max(c2, key=lambda x:c[x]))[0]
+                if max_card != 'J':
+                    thc = ("".join(thc)).replace("J",max_card)
+                elif max_card == 'J':
+                    thc = ("".join(thc)).replace("J",second_max_card)
             multivals = list(dict_to_odict(check_freq(thc)).values())
             self.name = types_of_hands[str(multivals)]
         else:
@@ -59,13 +79,19 @@ class Hand:
     def get_multi_values(self) -> list:
         return sorted(self.multiplicity.values(), reverse=True)
         
-    # def set_hand_name_part1(self) -> None:
-    #     self.name = types_of_hands[str(self.get_multi_values())]
+    def set_hand_name(self) -> None:
+        self.name = types_of_hands[str(self.get_multi_values())]
         
       
 @dataclass
 class Deck:
     hands: List[Hand]
+
+def removekey(d: dict, key:str) -> dict:
+    r = dict(d)
+    del r[key]
+    return r
+
 
 def check_freq(x: str) -> dict:
     freq = dict()
@@ -85,7 +111,7 @@ def input_file(inputfile: str) -> dict:
             res[i] = line
     return res
     
-def sort_hands(hands: List[Hand]) -> List[Hand]:
+def sort_hands(hands: List[Hand], cards: dict) -> List[Hand]:
      return sorted(hands, key= lambda x: (
             x.name[1],
             cards[x.content[0]],
@@ -103,7 +129,7 @@ def total_winnings(sorted_hands: List[Hand]) -> int:
     return sum
 
 def main():
-    data = input_file("./inputsmall.txt")
+    data = input_file("./input.txt")
 
     my_deck = list()
     for i,d in enumerate(data):
@@ -111,22 +137,22 @@ def main():
 
     deck = Deck(my_deck)
 
+    # SOLUTION OF FIRST PART
     for h in deck.hands:
         h.set_hand_multiplicity()
         h.set_hand_name()
         
     hands = deck.hands
-
-    # SOLUTION OF FIRST PART
-    sorted_hands = sort_hands(hands)
-
+    sorted_hands = sort_hands(hands, cards)
     print(total_winnings(sorted_hands))
     
     # SOLUTION OF SECOND PART
-    h=Hand("","KTJJT",684,{},0)
-    h.set_hand_multiplicity()
-    h.set_hand_name()
-    print(h)
+    for h in deck.hands:
+        h.set_hand_multiplicity()
+        h.set_hand_name_J()
+
+    sorted_hands = sort_hands(deck.hands, j_cards)
+    print(total_winnings(sorted_hands))
 
 if __name__ == "__main__":
     main()
