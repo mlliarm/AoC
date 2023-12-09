f0875277bf6c5d7298b48e8ab1aa065f3a38a73b
milia <mlliarm@users.noreply.github.com>
2023-12-09 20:54:08 +0100

    part one

--- 2023/Day09/extrapolate.py
+++ 2023/Day09/extrapolate.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 from typing import List, Dict, Tuple
 from dataclasses import dataclass
+from numpy import diff, array, ediff1d
 
 def input_file(inputfile: str) -> dict:
     with open(inputfile) as f:
@@ -12,31 +13,33 @@ def input_file(inputfile: str) -> dict:
             res[i] = line
     return res
 
-def get_new_sequences(l: List) -> (List):
+def get_new_sequences(l: List) -> int:
     tmpl = list()
-    if set(l) != {0}:   
-        for i,x in enumerate(l):
-            if i < len(l)-1:
-                tmp = l[i+1]
-                tmpl.append(tmp-x)
-                # print("tmp, tmp-x, tmpl: ", tmp, tmp-x,tmpl)
-        l = l + [tmp+tmpl[-1]]
-        print(l)
-        return get_new_sequences(tmpl)
-    else:
+    l = array(l)
+    while set(l) != {0}:
         tmpl.append(l)
-        print(tmpl[0]+[0])
-        # pass
-    # return l
-    
+        l = diff(l)
+    tmpl.append(array([0]*(len(l))))
 
-def main():
-    data = input_file("inputsmall.txt")
-    
+    sum = 0
+    for x in tmpl:
+        sum += x[-1]
+    return sum
+
+def PartOneSolution(data: Dict) -> int:
+    res = 0
     for k,v in enumerate(data):
         l = data[k]
-        get_new_sequences(l)
-        print(" ")
+        r = get_new_sequences(l)
+        res += r
+    return res
+
+def main():
+    data = input_file("input.txt")
+    
+    
+    # Part One 
+    print(PartOneSolution(data))
 
 if __name__ == "__main__":
     main()
